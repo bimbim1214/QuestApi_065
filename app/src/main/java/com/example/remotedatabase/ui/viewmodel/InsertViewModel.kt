d2a5314a0418805a9abf5451bef941742d25cce8
bimbim1214 <bimoadityapangestu@gmail.com>
2024-12-30 06:14:35 +0700

    Menambahkan ekstensi fungsi untuk mengonversi Mahasiswa menjadi InsertUiEvent

---
+++ app/src/main/java/com/example/remotedatabase/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.remotedatabase.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.remotedatabase.model.Mahasiswa
+import com.example.remotedatabase.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
