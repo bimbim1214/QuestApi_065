c5e42fc5c15153f6b0d3f22e44445232fa48ce85
bimbim1214 <bimoadityapangestu@gmail.com>
2024-12-30 06:44:47 +0700

    Menambahkan kelas data DetailUiState untuk mengelola status tampilan detail mahasiswa

--- app/src/main/java/com/example/remotedatabase/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/remotedatabase/ui/viewmodel/DetailViewModel.kt
@@ -9,6 +9,16 @@ import com.example.remotedatabase.ui.viewmodel.InsertUiEvent
 import kotlinx.coroutines.launch
 
 
+data class DetailUiState(
+    val detailUiEvent: InsertUiEvent = InsertUiEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = "",
+) {
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != InsertUiEvent()
+}
+
 fun Mahasiswa.toDetailUiEvent(): InsertUiEvent {
     return InsertUiEvent(
         nim = nim,
