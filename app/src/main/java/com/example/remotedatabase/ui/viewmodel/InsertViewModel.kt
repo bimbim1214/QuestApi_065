b2cf3f6c5c6c4ea7294fb80a416ff8f374ec5ec9
bimbim1214 <bimoadityapangestu@gmail.com>
2024-12-30 06:17:37 +0700

    Menambahkan InsertViewModel untuk mengelola status UI saat menambah data mahasiswa

--- app/src/main/java/com/example/remotedatabase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/remotedatabase/ui/viewmodel/InsertViewModel.kt
@@ -10,6 +10,28 @@ import com.example.remotedatabase.repository.MahasiswaRepository
 import kotlinx.coroutines.launch
 
 
+class InsertViewModel(private val mhs: MahasiswaRepository): ViewModel(){
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertMhs(){
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
 
 data class InsertUiEvent(
     val nim: String = "",
