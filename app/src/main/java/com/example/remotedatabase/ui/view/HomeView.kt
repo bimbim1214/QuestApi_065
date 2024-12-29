e004e92327d8ce3766d561a200fc7373e731ae85
bimbim1214 <bimoadityapangestu@gmail.com>
2024-12-30 06:23:37 +0700

    Menambahkan HomeScreen composable untuk menampilkan layar utama dengan daftar mahasiswa, tombol tambah, dan opsi refresh

--- app/src/main/java/com/example/remotedatabase/ui/view/HomeView.kt
+++ app/src/main/java/com/example/remotedatabase/ui/view/HomeView.kt
@@ -44,7 +44,59 @@ import com.example.remotedatabase.ui.viewmodel.HomeUiState
 import com.example.remotedatabase.ui.viewmodel.HomeViewModel
 
 
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = {},
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiHome.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getMhs()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Kontak"
+                )
+            }
+        },
+    ) { innerPadding ->
+        HomeStatus(
+            homeUiState = viewModel.mhsUIState,
+            retryAction = { viewModel.getMhs()},
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,
+            onDeleteClick = {
+                viewModel.deleteMhs(it.nim)
+                viewModel.getMhs()
+            }
+        )
+    }
+}
 
 @Composable
 fun HomeStatus(
